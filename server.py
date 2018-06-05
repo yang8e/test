b68bc34e23741c5b411b64ce4998776ce1fe144c
yang8e <yangxiaocheng@live.cn>
2018-06-05 14:29:17 +0800

    Update server.py

--- server.py
+++ server.py
@@ -5,12 +5,13 @@ import sys
 
 def nmap_scan():
     os.system('nmap -T4 -p23,22,80,443,445,873,3128,3306,1433,4848,4440,6082,6379,7001,7021,7080,7474,7755,7766,7888,8060,8880,8000,8881,8008,8080,8081,8087,8443,8090,8099,8088,8882,8883,8884,8885,8886,8887,8888,9043,9080,9090,9200,10000,15672,18080,11211,27017,50000 --open -oG nmap_out.txt -iL url1.txt')
-    fo=open('url_list.txt','r')
-    st=open('url_port.txt','w')
-    while 1:
-        line = fo.readline()
-        if not line:
-            break
+    list_nmap = []
+    st = open('url1.txt','w')
+    fo=open('nmap_out.txt','r')
+    for i in fo.readlines():
+        list_nmap.append(i)
+    get_list = list(set(list_nmap))
+    for line in get_list:
         if 'http' in line:
             ip = re.compile(r'(\d)*\.(\d)*\.(\d)*\.(\d)*')
             find_ip = ip.search(line)
